71cb9a92db5f9d21ebb75d1434a56c630c5eebeb
elena <elena@MacBook-Pro-de-elena.local>
2020-04-16 19:05:08 -0300

    recuperar

--- models/insumo.js
+++ models/insumo.js
@@ -7,10 +7,10 @@ var insumoSchema =	new Schema({
     tipo: { type: String, required: [true, 'Ingrese el tipo de repusto'] },
     marca: { type: String, required:false },
     detalle: {	type: String,	required: [true,	'El	Detalle es necesario']	},
-    costo: {	type: Number,	required: [true,	'El	costo es necesario']},
+    costo: {	type: Number,	required: false	},
     stockMinimo: { type: Number,	required: false	},    
-	porcAplicado: {	type: Number,	required: [true,	'El	porcentaje es necesario']	},
-    precio: {	type: Number,	required: false	},
+	porcAplicado: {	type: Number,	required: false	},
+    precio: {	type: String,	required: false	},
     stock: { type: Number,	required: [true, 'El stock es necesario']	},
     eliminado: { type: Boolean,	required: false,default: false	},
     proveedor: { type: Schema.Types.ObjectId,    ref: 'Proveedor',  required: false }
